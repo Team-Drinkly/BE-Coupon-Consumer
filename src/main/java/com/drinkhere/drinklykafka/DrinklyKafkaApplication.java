463b0c0071f0dee083ea115b821358c5483a4343
안정후 <austinan@naver.com>
2025-02-14 14:45:20 +0900

    chore: kafka consumer server settings

---
+++ src/main/java/com/drinkhere/drinklykafka/DrinklyKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.drinkhere.drinklykafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinklyKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrinklyKafkaApplication.class, args);
+    }
+
+}
